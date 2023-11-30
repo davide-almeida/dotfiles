0be963c6b2b013dbc61c739606ddfe380d99e443
Davide Almeida <davide.jhonatan@gmail.com>
2023-11-29 22:23:34 -0300

    add nix config

---
+++ home.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "davide";
+  home.homeDirectory = "/home/davide";
+
+  home.stateVersion = "23.11";
+
+  home.packages = with pkgs; [
+   ruby_3_2
+
+  ];
+
+  home.file = {
+
+  };
+
+  home.sessionVariables = {
+  };
+
+  programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "Davide Almeida";
+    userEmail = "davide.jhonatan@gmail.com";
+  };
+}
